ec21bb19cdbe4f6311aa4e932a1f18064992fbd3
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-04-09 23:10:29 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap <String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            char ch [] = s.toCharArray();
+            Arrays.sort(ch);
+
+            String sortedWord = new String(ch);
+
+            if(!map.containsKey(sortedWord)){
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(s);
+
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
