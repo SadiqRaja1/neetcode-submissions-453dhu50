17de393cfa345bab409bebceb696affa38d6570b
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-04-10 08:16:15 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+
+        if(strs.isEmpty()){
+            return "";
+        }
+
+        StringBuilder res = new StringBuilder();
+        List<Integer> sizes = new ArrayList<>();
+
+        for ( String st : strs) {
+            sizes.add(st.length());
+        }
+
+        for (int size : sizes) {
+            res.append(size).append(",");
+        }
+
+        res.append("#");
+
+        for (String st : strs) {
+            res.append(st);
+        }
+
+        return res.toString();
+
+    }
+
+    public List<String> decode(String str) {
+
+        if(str.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        List<String> res = new ArrayList<>();
+        List<Integer> sizes = new ArrayList<>();
+
+        int i = 0;
+
+        while(str.charAt(i) != '#') {
+            StringBuilder curr = new StringBuilder();
+            while (str.charAt(i) != ','){
+                curr.append(str.charAt(i));
+                i++;
+            }
+            sizes.add(Integer.parseInt(curr.toString()));
+
+            i++;
+        }
+        i++;
+        
+        for(int sz : sizes){
+            res.add(str.substring(i,i+sz));
+            i += sz;
+        }
+
+        return res;
+
+
+    }
+}
