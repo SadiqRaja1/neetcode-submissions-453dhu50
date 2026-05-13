960b822470e7885525854d8f60aaff1fdc985ef2
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-05-13 23:03:29 +0530

    Add: valid-sudoku - submission-9

---
+++ Data Structures & Algorithms/valid-sudoku/submission-9.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int row = 0; row<9; row++) {
+            HashSet<Character> hash = new HashSet<>();
+            for(int col = 0; col<9; col++){
+                if(board[row][col] == '.') continue;
+                if(hash.contains(board[row][col])) return false;
+                hash.add(board[row][col]);
+            }
+        }
+
+        for(int col=0; col<9; col++){
+            HashSet<Character> hash = new HashSet<>();
+            for(int row=0; row<9; row++) {
+                if(board[row][col] == '.') continue;
+                if(hash.contains(board[row][col])) return false;
+                hash.add(board[row][col]);
+            }
+        }
+
+        for(int start = 0; start < 9; start++) {
+            HashSet<Character> hash = new HashSet<>();
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    int row = (start / 3)*3 + i;
+                    int col = (start % 3)*3 + j;
+                    if(board[row][col] == '.') continue;
+                    if(hash.contains(board[row][col])) return false;
+                    hash.add(board[row][col]);
+                }
+            }
+        }
+
+        return true;
+    }
+}
