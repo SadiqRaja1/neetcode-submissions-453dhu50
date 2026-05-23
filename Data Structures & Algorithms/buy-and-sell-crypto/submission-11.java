7b2dd7bab35870f7059950cad13e01306447047b
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-05-23 20:59:46 +0530

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int buying = prices[0];
+        int max = 0;
+
+        for (int r=1; r<n; r++) {
+            if(prices[r] < buying){
+                buying = prices[r];
+                continue;
+            }
+            max = Math.max(max, prices[r]-buying);
+        }
+
+        return max;
+    }
+}
