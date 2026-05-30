aec37320f9d7f31ff0ba8f1d7255301876c187db
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-05-30 07:26:13 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int answer [] = new int[n];
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(temperatures[i] < temperatures[j]){
+                    answer[i] = j-i;
+                    break;
+                }else{
+                    answer[i] = 0;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+}
