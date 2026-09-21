3029b516e9d7a75da5a7e5d708989da4d668db96
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-09-21 17:01:51 +0530

    Add: valid-sudoku - submission-16

---
+++ Data Structures & Algorithms/valid-sudoku/submission-16.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int row=0; row<9; row++) {
+            HashSet<Character> isPresent = new HashSet<>();
+            for(int col=0; col<9; col++) {
+                if(board[row][col] == '.') continue;
+                if(isPresent.contains(board[row][col])) return false;
+                isPresent.add(board[row][col]);
+            }
+        }
+        for(int col=0; col<9; col++) {
+            HashSet<Character> isPresent = new HashSet<>();
+            for(int row=0; row<9; row++) {
+                if(board[row][col] == '.') continue;
+                if(isPresent.contains(board[row][col])) return false;
+                isPresent.add(board[row][col]);
+            }
+        }
+
+        for(int grid=0; grid<9; grid++) {
+            HashSet<Character> isPresent = new HashSet<>();
+            for(int i=0; i<3; i++) {
+                for(int j=0; j<3; j++) {
+                    int row = (grid/3)*3+i;
+                    int col = (grid%3)*3+j;
+                    if(board[row][col] == '.') continue;
+                    if(isPresent.contains(board[row][col])) return false;
+                    isPresent.add(board[row][col]);
+                }
+            }
+        }
+        return true;
+    }
+}
