72743a733fb70cec7bca62a1437c1aef68000d9a
Shaikh Sadiq Raja Sardar <113375636+SadiqRaja1@users.noreply.github.com>
2026-09-22 14:44:52 +0530

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int start = 0;
+        int end = s.length()-1;
+        while(start < end) {
+            if(s.charAt(start++) != s.charAt(end--)) return false;
+        }
+        return true;
+    }
+}
